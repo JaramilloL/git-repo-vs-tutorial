6e4b7a28bfcd67ab343135ae4ccb8de896f3a8e5
JLJL <juanluisjaramillolopez@gmail.com>
2023-10-18 17:02:32 -0600

    esta es la segunda modificacion a App.jsx, se agrego una imagen

--- git-repo/src/App.jsx
+++ git-repo/src/App.jsx
@@ -1,7 +1,9 @@
+import imagen from './assets/react.svg'
 const App = () => {
   return (
     <div>
       <h1>Estamos usando Git y Github</h1>
+      <img src={imagen} alt="" />
     </div>
   )
 }
